81446558ee7394199f5f262278021bb796ec838e
Noah Rolf <noahrolf1@gmail.com>
2017-08-04 15:38:54 -0500

    janky working

--- src/app/index.js
+++ src/app/index.js
@@ -1,32 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render } from "react-dom";
+import {
+	render
+} from "react-dom";
 
 let inventory = [];
+let balance = 100;
+let gameStop = true;
+let clickCounter = 0;
+let expire = false;
 
-function buyItem() {
-	console.log(this.index);
+function startGame() {
+	console.log('start');
+	for (let item of inventory) {
+		balance = 100;
+		gameStop = false;
+		console.log('item');
+		item.setPrice();
+		item.changePrice();
+	};
+	setTimeout(function() {
+		gameStop = true;
+		alert('Game Over!');
+	}, 900000);
 }
 
-function sellItem() {
-	console.log(this.index);
+function buyItem(i) {
+  console.log('buy', i, gameStop);
+	if (!gameStop) {
+		let item = inventory[i];
+		if (balance >= item.price) {
+			balance -= item.price;
+			item.owned++;
+			averager(i)
+		}
+		clickCounter++;
+	}
+}
+
+function sellItem(i) {
+  console.log('sell', i, gameStop);
+	if (!gameStop) {
+		let item = inventory[i];
+		if (item.owned > 0) {
+			item.owned--;
+			balance += item.price;
+		}
+		clickCounter++;
+	}
+}
+
+function averager(i) {
+	let item = inventory[i];
+	item.pricesArray.push(item.price);
+	let sum = item.pricesArray.reduce((previous, current) => current += previous);
+	item.averageDisplay = sum / item.pricesArray.length;
 }
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="col s10 offset-s1 center-align">
-						<h1>Hello!</h1>
-					</div>
-				</div>
-			</div>
+      <div className="container">
+			   <div className="row">
+			      <div className="col s10 offset-s1 center-align">
+			         <h1> Buy Commodities! </h1>
+               <button className = "btn" onClick = {() => startGame()}> start game </button>
+            </div>
+        </div>
+      </div >
 		);
 	}
 }
 
-render(<App />, window.document.getElementById("app"));
+render( < App / > , window.document.getElementById("app"));
 
 class Commodity {
 	constructor(name, img, category) {
@@ -42,7 +88,7 @@ class Commodity {
 		this.price += randomNumber(0.5, 9.99);
 	}
 	changePrice() {
-		$interval(() => {
+		setInterval(() => {
 			let x = randomNumber(0, 9);
 			if (x >= 5) {
 				let z = randomNumber(0.1, 0.9);
@@ -59,6 +105,7 @@ class Commodity {
 					this.price += randomNumber(0.1, 0.9);
 				}
 			}
+			console.log(this.price);
 		}, 15000);
 	}
 } //class end
@@ -73,15 +120,7 @@ class Fruit extends Commodity {
 	changePrice() {
 		super.changePrice();
 	}
-	expire() {
-		if (this.category == "fruit") {
-			if (vm.clickCounter >= 10) {
-				expire = true;
-				vm.clickCounter = 0;
-			}
-		}
-	}
-}
+};
 
 class Elec extends Commodity {
 	constructor(name, img, category) {
@@ -91,17 +130,18 @@ class Elec extends Commodity {
 		super.setPrice();
 	}
 	changePrice() {
-		$interval(() => {
+		setInterval(() => {
 			let z = randomNumber(0.1, 0.3);
 			this.price += z;
 		}, 15000);
 	}
-}
+};
 
 class Collect extends Commodity {}
 
 function randomNumber(min, max) {
-	return (mathNumber = Math.random() * (max - min));
+	let mathNumber = Math.random() * (max - min);
+	return mathNumber;
 }
 
 const fruitArray = [
@@ -139,40 +179,27 @@ for (var i = 0; i < inventory.length; i++) {
 	inventory[i].index = i;
 }
 
-const Test = ({ inventory }) =>
-	<div className="row">
-		{inventory.map((item, i) =>
-			<div className="item" key={"item_" + i}>
-				<div className="card hoverable col s3">
-					<img onClick={buyItem} src={item.img} alt="" />
-					<div className="container center-align">
-						<h4 className="">
-							{item.name}
-						</h4>
-						<p>
-							Price: {item.price}
-						</p>
-						<p>
-							Average price paid: {item.averageDisplay}
-						</p>
-						<p>
-							Amount owned: {item.owned}
-						</p>
-						<button
-							className="btn"
-							data={item.index}
-							onClick={sellItem}
-							name="button"
-						>
-							Sell
-						</button>
-					</div>
-				</div>
-			</div>
-		)}
-	</div>;
+setInterval(function(){
+  var Test = ({inventory}) =>
+    <div className = "row" > {
+      inventory.map((item, i) =>
+        <div className="item"key = {"item_" + i}>
+           <div className="card col s3">
+              <img src = {item.img}alt = ""/>
+              <div className="container center-align">
+                 <h4 className=""> {item.name}</h4>
+                 <p>Price: {item.price} </p>
+                 <p>Average price paid: {item.averageDisplay}</p>
+                 <p>Amount owned: {item.owned}</p>
+                 <button className="btn" onClick={() => sellItem(i)} name = "button" >Sell </button>
+                 <button className="btn" onClick={() => buyItem(i)} name = "button" >Buy </button>
+              </div>
+           </div>
+        </div>
+      )
+    }
+  </div>;
 
-ReactDOM.render(
-	<Test inventory={inventory} />,
-	document.getElementById("container")
-);
+  ReactDOM.render( <Test inventory = {inventory}/>, document.getElementById("container"));
+    ( <Test inventory = {inventory} />, document.getElementById("container") );
+}, 500);
